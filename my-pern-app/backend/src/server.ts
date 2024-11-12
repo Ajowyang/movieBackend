c2544dc33cd452ce90b357ea10e6226f7ed906bf
ajowyang <andrewowyang875@gmail.com>
2024-11-12 13:21:58 -0800

    made changes saving progress

--- my-pern-app/backend/src/server.ts
+++ my-pern-app/backend/src/server.ts
@@ -1,30 +1,36 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { Pool } from 'pg';
 import pg from 'pg'
+// import { ClientError, errorMiddleware } from './lib/index.js';
 
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
-const pool = new Pool({  user: process.env.DB_USER,  host: process.env.DB_HOST,  database: process.env.DB_NAME,  password: process.env.DB_PASSWORD,  port: Number(process.env.DB_PORT) || 5432,});
+// const pool = new Pool({  user: process.env.DB_USER,  host: process.env.DB_HOST,  database: process.env.DB_NAME,  password: process.env.DB_PASSWORD,  port: Number(process.env.DB_PORT) || 5432,});
 const db = new pg.Pool({
-    connectionString: 'postgres://andrewowyang:@localhost/movieDb',
+    connectionString: process.env.DATABASE_URL,
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // }
   });
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 
 app.get('/api', (req, res) => {  res.send('Helo from the backend!');    
 });
-app.listen(port, () => {  console.log(`Server is running on http://localhost:${port}`);
-});
+
 
 app.post('/api/createMovie', async (req, res, next)=>{
     try{
         const { title, summary, imdbLink, rating} = req.body;
+        if(typeof rating !== 'number'){
+            // throw new ClientError(400, `rating must be integer`);
+        }
+
         const sql = `
         insert into "movies" ( "title", "summary", "imdbLink", "rating" )
         values($1, $2, $3, $4)
@@ -35,7 +41,7 @@ app.post('/api/createMovie', async (req, res, next)=>{
     }
     catch(err){
         next(err)
-    }
+    }//return status 500 response on my own
 })
 
 app.put('/api/updateMovie/:movieId', async (req, res, next)=>{
@@ -87,3 +93,6 @@ app.get('/api/getMovies', async(req, res, next)=>{
         next(err)
     }
 })
+
+app.listen(port, () => {  console.log(`Server is running on http://localhost:${port}`);
+});
